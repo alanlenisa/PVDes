ca8f0cddf3bd3eeff77303e1746dd64984fe952f
alan lenisa <alan.lenisa@gmail.com>
2023-07-03 16:13:19 +0200

    230702

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import logo from './sun_logo.png';
 import './App.css';
 import { useCurrentLanguage } from "./hooks/useLanguage";
+import Mapper from './components/Mapper';
 
 function App() {
     const T = useCurrentLanguage();
 
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-                    <p className="title" >{T("ui_welcome")}</p>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p className="title" >{T("ui_welcome")}</p>
         
        
-      </header>
-    </div>
+            </header>
+        <Mapper/>
+        </div>
   );
 }
 
